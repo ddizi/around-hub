3f7a57dda5a3dee8aa2c9ef6f09ed7beecde148d
ddizi <ruroo@naver.com>
2023-07-15 11:10:54 +0900

    first commit

---
+++ src/test/java/com/mj/around/hub/AroundHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj.around.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
