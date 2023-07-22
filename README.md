b759572d4d457cf1a177d35372c2725d69795d09
ddizi <ruroo@naver.com>
2023-07-22 10:05:12 +0900

    first controller

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # around-hub
 Around HUB SpringBoot Study
+
+## Status
+7/51 ing (2021.07.01)
+
+## Talend API Tester
+
+간단히 API를 테스트할 수 있는 크롬 확장 프로그램
+
--- src/main/java/com/mj/around/hub/controller/HelloController.java
+++ src/main/java/com/mj/around/hub/controller/HelloController.java
@@ -1,2 +1,13 @@
-package com.mj.around.hub.controller;public class HelloController {
+package com.mj.around.hub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
